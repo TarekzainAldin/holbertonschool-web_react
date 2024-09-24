6ee4d8c6623f421e6df9cb5e2c2caf57f173f68e
TAREKZAINALDIN <tarekzindin@gmail.com>
2024-09-24 12:19:31 +0200

    make the print teacher

--- task_1/js/main.ts
+++ task_1/js/main.ts
@@ -1,47 +1,44 @@
-// Define the Teacher interface
-interface Teacher {
-    readonly firstName: string;       // can only be set during initialization
-    readonly lastName: string;        // can only be set during initialization
-    fullTimeEmployee: boolean;
-    yearsOfExperience?: number;      // optional
-    location: string;
-    [key: string]: any;               // allows any other property to be added dynamically
-}
-
-// Create a class that implements the Teacher interface
-class SchoolTeacher implements Teacher {
-    // Define properties
+export interface Teacher {
     readonly firstName: string;
     readonly lastName: string;
     fullTimeEmployee: boolean;
-    location: string;
     yearsOfExperience?: number;
-
-    constructor(
-        firstName: string,
-        lastName: string,
-        fullTimeEmployee: boolean,
-        location: string,
-        yearsOfExperience?: number,
-        additionalAttributes?: { [key: string]: any }
-    ) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.fullTimeEmployee = fullTimeEmployee;
-        this.location = location;
-        this.yearsOfExperience = yearsOfExperience;
-
-        // Dynamically add additional attributes to the object
-        if (additionalAttributes) {
-            Object.assign(this, additionalAttributes);
-        }
-    }
+    location: string;
+    [propName: string]: any;
 }
 
-// Example usage:
-const teacher1 = new SchoolTeacher("Alice", "Smith", true, "New York", 5, { contract: true });
-const teacher2 = new SchoolTeacher("Bob", "Johnson", false, "California", undefined, { department: "Math" });
+export interface Directors extends Teacher {
+    numberOfReports: number;
+}
 
-// Log the created Teacher objects
-console.log(teacher1);
-console.log(teacher2);
+export interface printTeacherFunction {
+    (firstName: string, lastName: string): string;
+}
+
+export const printTeacher: printTeacherFunction = function (firstName: string, lastName: string) {
+    return `${firstName.slice(0, 1)}. ${lastName}`;
+}
+
+interface StudentConstructor {
+    new(firstName: string, lastName: string): StudentClassInterface;
+}
+
+interface StudentClassInterface {
+    workOnHomework(): string;
+    displayName(): string;
+}
+
+export const StudentClass: StudentConstructor = class StudentClass implements StudentClassInterface {
+    firstName: string;
+    lastName: string;
+    constructor(firstName: string, lastName: string) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    workOnHomework(): string {
+        return 'Currently working';
+    }
+    displayName(): string {
+        return this.firstName;
+    }
+}
