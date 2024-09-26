a286c70898e97395d285fa9319ff9b32802c7f68
TAREKZAINALDIN <tarekzindin@gmail.com>
2024-09-26 10:12:38 +0200

    make the task4 crud and interface and import in the main .ts

---
+++ task_3/js/crud.d.ts
@@ -0,0 +1,4 @@
+  import { RowID,RowElement } from "./interface";
+  declare function insertRow(row:RowElement):number;
+  declare function deleteRow(rowID:RowID):void;
+  declare function updateRow(rowID:RowID,row:RowElement):number;
---
+++ task_3/js/main.ts
@@ -0,0 +1,17 @@
+///<refernce path = "./crud.d.ts"/>
+import { RowID,RowElement } from "./interface";
+import * as CRUD from './crud.js';
+
+
+const row : RowElement = {
+    firstName:'Guilaume' , 
+    lastName: 'Salava'
+}
+
+const newRowID: RowID = CRUD.insertRow(row)
+const updateRow:RowElement={...row,age:23};
+CRUD.updateRow(newRowID,updateRow);
+CRUD.deleteRow(newRowID);
+
+
+//comment >>>"./crud.d.ts/>"
