c5de15f97d98a347e51af25f329d1186945addfe
TAREKZAINALDIN <tarekzindin@gmail.com>
2025-04-16 17:17:52 +0200

    adding teh lib weebpack after fix the error by chat gpt

--- TypeScript/task_4/dist/bundle.js
+++ TypeScript/task_4/dist/bundle.js
@@ -49,12 +49,6 @@ var Subjects;
 /***/ })
 
 /******/ 	});
-/************************************************************************/
-/******/ 	
-/******/ 	// startup
-/******/ 	// Load entry module and return exports
-/******/ 	// This entry module is referenced by other modules so it can't be inlined
-/******/ 	var __webpack_exports__ = {};
 /******/ 	__webpack_modules__["./js/subjects/Cpp.ts"]();
 /******/ 	
 /******/ })()
