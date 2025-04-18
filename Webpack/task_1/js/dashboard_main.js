0f610bd2aaf12e46f62b939f76c450bdbacc87db
TAREKZAINALDIN <tarekzindin@gmail.com>
2025-04-18 12:37:23 +0200

    now

--- Webpack/task_1/js/dashboard_main.js
+++ Webpack/task_1/js/dashboard_main.js
@@ -1,19 +1,21 @@
-import $ from 'jquery';
-import _ from 'lodash';
+import $ from "jquery";
+import _ from "lodash";
 
-$(document).ready(function () {
-  $('body').append('<p>Holberton Dashboard</p>');
-  $('body').append('<p>Dashboard data for the students</p>');
-  $('body').append('<button>Click here to get started</button>');
-  $('body').append('<p id="count"></p>');
-  $('body').append('<p>Copyright - Holberton School</p>');
+let count = 0;
 
-  let count = 0;
+function updateCounter() {
+    count += 1;
+    $("#count").text(`${count} clicks on the button`);
+}
 
-  function updateCounter() {
-    count++;
-    $('#count').text(`${count} clicks on the button`);
-  }
+// version "debounced" de updateCounter avec un délai de 500ms
+const debouncedUpdateCounter = _.debounce(updateCounter, 500);
 
-  $('button').on('click', _.debounce(updateCounter, 500));
-});
+$("body").append("<p>Holberton Dashboard</p>");
+$("body").append("<p>Dashboard data for the students</p>");
+$("body").append("<button id='clickButton'>Click here to get started</button>");
+$("body").append("<p id='count'></p>");
+$("body").append("<p>Copyright - Holberton School</p>");
+
+// Attacher l'événement click du bouton à la version "debounced" de updateCounter
+$("#clickButton").on("click", debouncedUpdateCounter);
--- Webpack/task_1/webpack.config.js
+++ Webpack/task_1/webpack.config.js
@@ -4,8 +4,21 @@ module.exports = {
   entry: './js/dashboard_main.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'public'),
+    path: path.resolve(__dirname, 'public')
   },
   mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  }
 };
-
