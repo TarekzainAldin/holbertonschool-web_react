f94e8bf6d09afbaed7c774613225af93f001975e
TAREKZAINALDIN <tarekzindin@gmail.com>
2025-04-18 13:36:15 +0200

    its good phohto now

--- Webpack/task_2/js/dashboard_main.js
+++ Webpack/task_2/js/dashboard_main.js
@@ -1,23 +1,23 @@
-import $ from 'jquery';
-import _ from 'lodash';
+import $ from "jquery";
+import _ from "lodash";
 import '../css/main.css';
 
 let count = 0;
 
 function updateCounter() {
-  count++;
-  $('#count').text(`${count} clicks on the button`);
+    count += 1;
+    $("#count").text(`${count} clicks on the button`);
 }
 
-const debounced = _.debounce(updateCounter, 500);
+// version "debounced" de updateCounter avec un délai de 500ms
+const debouncedUpdateCounter = _.debounce(updateCounter, 500);
 
-$(document).ready(function () {
-  $('body').append('<div id="logo"></div>');
-  $('body').append('<p>Holberton Dashboard</p>');
-  $('body').append('<p>Dashboard data for the students</p>');
-  $('body').append('<button>Click here to get started</button>');
-  $('body').append("<p id='count'></p>");
-  $('body').append('<p>Copyright - Holberton School</p>');
+$('body').append('<div id="logo"></div>');
+$("body").append("<p>Holberton Dashboard</p>");
+$("body").append("<p>Dashboard data for the students</p>");
+$('body').append('<button>Click here to get started</button>');
+$("body").append("<p id='count'></p>");
+$("body").append("<p>Copyright - Holberton School</p>");
 
-  $('button').on('click', debounced);
-});
+// Attacher l'événement click du bouton à la version "debounced" de updateCounter
+$('button').on("click", debouncedUpdateCounter);
