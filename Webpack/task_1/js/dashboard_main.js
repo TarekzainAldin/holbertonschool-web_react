10d62277fea610ed85219df0c76eeae238699929
TAREKZAINALDIN <tarekzindin@gmail.com>
2025-04-19 09:37:47 +0200

    try now

--- Webpack/task_1/js/dashboard_main.js
+++ Webpack/task_1/js/dashboard_main.js
@@ -1,21 +1,16 @@
-import $ from "jquery";
-import _ from "lodash";
+import $ from 'jquery';
+import _ from 'lodash';
+
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<button id="click-me">Click here to get started</button>');
+$('body').append('<p id="count"></p>');
+$('body').append('<p>Copyright - Holberton School</p>');
 
 let count = 0;
-
 function updateCounter() {
-    count += 1;
-    $("#count").text(`${count} clicks on the button`);
+  count += 1;
+  $('#count').text(`${count} clicks on the button`);
 }
 
-// version "debounced" de updateCounter avec un délai de 500ms
-const debouncedUpdateCounter = _.debounce(updateCounter, 500);
-
-$("body").append("<p>Holberton Dashboard</p>");
-$("body").append("<p>Dashboard data for the students</p>");
-$("body").append("<button id='clickButton'>Click here to get started</button>");
-$("body").append("<p id='count'></p>");
-$("body").append("<p>Copyright - Holberton School</p>");
-
-// Attacher l'événement click du bouton à la version "debounced" de updateCounter
-$("#clickButton").on("click", debouncedUpdateCounter);
+$('#click-me').on('click', _.debounce(updateCounter, 500));
--- Webpack/task_1/webpack.config.js
+++ Webpack/task_1/webpack.config.js
@@ -1,24 +1,10 @@
 const path = require('path');
 
 module.exports = {
+  mode: 'production',
   entry: './js/dashboard_main.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'public')
+    path: path.resolve(__dirname, 'public'),
   },
-  mode: 'production',
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env']
-          }
-        }
-      }
-    ]
-  }
 };
