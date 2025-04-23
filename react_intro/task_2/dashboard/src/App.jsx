ad980dc74aa4f52fdad1da6aab2d50b9efdba3f6
TAREKZAINALDIN <tarekzindin@gmail.com>
2025-04-23 18:48:07 +0200

    make lable and input for the email and password with the button ok with the foucs cursore on the input when click in the lable

--- react_intro/task_2/dashboard/src/App.jsx
+++ react_intro/task_2/dashboard/src/App.jsx
@@ -9,13 +9,24 @@ function App() {
       <div className="root-notifications">
         <Notifications />
       </div>
+
       <div className="App-header">
         <img src={logo} alt="holberton logo" />
         <h1>School dashboard</h1>
       </div>
+
       <div className="App-body">
         <p>Login to access the full dashboard</p>
+
+        <label htmlFor="email">Email:</label>
+        <input type="email" id="email" name="email" />
+
+        <label htmlFor="password">Password:</label>
+        <input type="password" id="password" name="password" />
+
+        <button>OK</button>
       </div>
+
       <div className="App-footer">
         <p>
           Copyright {getCurrentYear()} - {getFooterCopy(true)}
