63bfd04201ee11326f8601c22250741a69d4d419
TAREKZAINALDIN <tarekzindin@gmail.com>
2025-04-23 18:26:49 +0200

    make notification with the true value for is index

--- react_intro/task_1/dashboard/src/App.jsx
+++ react_intro/task_1/dashboard/src/App.jsx
@@ -1,20 +1,25 @@
-import holbertonLogo from './assets/holberton-logo.jpg';
-import './App.css';
+import "./App.css";
+import logo from "./assets/holberton-logo.jpg";
+import { getCurrentYear, getFooterCopy } from "./utils";
+import Notifications from "./Notifications";
 
 function App() {
-  const currentYear = new Date().getFullYear();
-
   return (
     <>
+      <div className="root-notifications">
+        <Notifications />
+      </div>
       <div className="App-header">
-        <img src={holbertonLogo} alt="holberton logo" />
+        <img src={logo} alt="holberton logo" />
         <h1>School dashboard</h1>
       </div>
       <div className="App-body">
         <p>Login to access the full dashboard</p>
       </div>
       <div className="App-footer">
-        <p>Copyright {currentYear} - holberton School</p>
+        <p>
+          Copyright {getCurrentYear()} - {getFooterCopy(true)}
+        </p>
       </div>
     </>
   );
