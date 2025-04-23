97ba1d860477abae91946e644fffe3e73e99e7d9
TAREKZAINALDIN <tarekzindin@gmail.com>
2025-04-23 19:37:04 +0200

    ;;;;;

--- react_intro/task_2/dashboard/src/App.jsx
+++ react_intro/task_2/dashboard/src/App.jsx
@@ -9,24 +9,19 @@ function App() {
       <div className="root-notifications">
         <Notifications />
       </div>
-
       <div className="App-header">
         <img src={logo} alt="holberton logo" />
         <h1>School dashboard</h1>
       </div>
-
       <div className="App-body">
         <p>Login to access the full dashboard</p>
 
         <label htmlFor="email">Email:</label>
         <input type="email" id="email" name="email" />
-
-        <label htmlFor="password">Password:</label>
+        <label htmlFor="password">password:</label>
         <input type="password" id="password" name="password" />
-
-        <button>OK</button>
+        <button type="submit">OK</button>
       </div>
-
       <div className="App-footer">
         <p>
           Copyright {getCurrentYear()} - {getFooterCopy(true)}
