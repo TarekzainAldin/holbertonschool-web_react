d3c35ef0382aa75c3c0d666d742bc9ca209a863c
TAREKZAINALDIN <tarekzindin@gmail.com>
2025-04-23 18:33:25 +0200

    copy the folder task 1 and make folder task 2

---
+++ react_intro/task_2/dashboard/src/Notifications.jsx
@@ -0,0 +1,15 @@
+//task_1/dashboard/src/notification.jsx
+
+import React from 'react';
+
+import "./Notifications.css"; 
+
+
+function Notifications(){
+    return (
+        <div className="notifications">
+            <p>here is the list of notifications</p>
+        </div>
+    );
+}
+export default Notifications;
