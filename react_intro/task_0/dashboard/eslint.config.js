c08de2ad653683e128bd218e1d1f1f1c3c882bdb
TAREKZAINALDIN <tarekzindin@gmail.com>
2025-04-23 17:03:36 +0200

    testing header body and footer

--- react_intro/task_0/dashboard/eslint.config.js
+++ react_intro/task_0/dashboard/eslint.config.js
@@ -1,7 +1,8 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import jestPlugin from 'eslint-plugin-jest';
 
 export default [
   { ignores: ['dist'] },
@@ -9,7 +10,10 @@ export default [
     files: ['**/*.{js,jsx}'],
     languageOptions: {
       ecmaVersion: 2020,
-      globals: globals.browser,
+      globals: {
+        ...globals.browser,
+        ...globals.jest, // Add Jest globals
+      },
       parserOptions: {
         ecmaVersion: 'latest',
         ecmaFeatures: { jsx: true },
@@ -19,10 +23,12 @@ export default [
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      'jest': jestPlugin, // Add Jest plugin
     },
     rules: {
       ...js.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
+      ...jestPlugin.configs.recommended.rules, // Add Jest recommended rules
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
       'react-refresh/only-export-components': [
         'warn',
@@ -30,4 +36,4 @@ export default [
       ],
     },
   },
-]
+];
