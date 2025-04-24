14c7f8eea09f419bf295a7629b704aefa8db419e
TAREKZAINALDIN <tarekzindin@gmail.com>
2025-04-24 12:22:13 +0200

    change the password from small later to capital later

--- react_intro/task_2/dashboard/src/App.jsx
+++ react_intro/task_2/dashboard/src/App.jsx
@@ -18,7 +18,7 @@ function App() {
 
         <label htmlFor="email">Email:</label>
         <input type="email" id="email" name="email" />
-        <label htmlFor="password">password:</label>
+        <label htmlFor="password">Password:</label>
         <input type="password" id="password" name="password" />
         <button type="submit">OK</button>
       </div>
