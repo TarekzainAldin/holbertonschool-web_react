f3d4f3c1e019f7eaaa280b7a5e5b5a1f241e8473
TAREKZAINALDIN <tarekzindin@gmail.com>
2025-04-25 10:55:57 +0200

    try anto

--- react_intro/task_0/dashboard/src/App.spec.js
+++ react_intro/task_0/dashboard/src/App.spec.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import '@testing-library/jest-dom';
 import App from './App';
 
 describe('App component', () => {
-  test('renders the main heading with text "School dashboard"', () => {
-    render(<App />);
-    const heading = screen.getByRole('heading', { name: /school dashboard/i });
+  test('renders the main heading', () => {
+    const { container } = render(<App />);
+    const heading = screen.getByRole('heading', {
+      level: 1,
+      name: /school dashboard/i,
+    });
     expect(heading).toBeInTheDocument();
   });
 
-  test('renders login and footer paragraphs correctly', () => {
+  test('renders the login and footer paragraphs', () => {
     render(<App />);
     const bodyText = screen.getByText(/login to access the full dashboard/i);
-    const footerText = screen.getByText(/copyright.*holberton school/i);
+    const footerText = screen.getByText(/copyright/i);
     expect(bodyText).toBeInTheDocument();
     expect(footerText).toBeInTheDocument();
   });
