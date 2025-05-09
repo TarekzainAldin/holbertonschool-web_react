24e8d5de4ad317759ac35b7cf5b2f620b443ed89
TAREKZAINALDIN <tarekzindin@gmail.com>
2025-05-09 16:36:57 +0200

    app

--- react_component/task_3/dashboard/src/App/App.jsx
+++ react_component/task_3/dashboard/src/App/App.jsx
@@ -1,35 +1,30 @@
-import React, { Component } from "react";
-import PropTypes from "prop-types";
+import React from "react";
 import "./App.css";
 import Notifications from "../Notifications/Notifications";
 import Header from "../Header/Header";
-import Login from "../Login/Login";
 import Footer from "../Footer/Footer";
+import Login from "../Login/Login";
+import PropTypes from "prop-types";
 import CourseList from "../CourseList/CourseList";
+import { getLatestNotification } from "../utils/utils";
+import BodySection from "../BodySection/BodySection";
+import BodySectionWithMarginBottom from "../BodySection/BodySectionWithMarginBottom";
 
-class App extends Component {
+class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      isLoggedIn: false,
-    };
-
-    this.notificationsList = [
-      { id: 1, type: "default", value: "New course available" },
-      { id: 2, type: "urgent", value: "New resume available" },
-      {
-        id: 3,
-        type: "urgent",
-        html: { __html: "<strong>Urgent requirement</strong> - complete by EOD" },
-      },
-    ];
-
-    this.coursesList = [
-      { id: 1, name: "ES6", credit: 60 },
-      { id: 2, name: "Webpack", credit: 20 },
-      { id: 3, name: "React", credit: 40 },
-    ];
+    this.handleKeyDown = this.handleKeyDown.bind(this);
   }
+
+  static defaultProps = {
+    logOut: () => {},
+    isLoggedIn: false,
+  };
+
+  static propTypes = {
+    logOut: PropTypes.func,
+    isLoggedIn: PropTypes.bool,
+  };
 
   componentDidMount() {
     document.addEventListener("keydown", this.handleKeyDown);
@@ -39,39 +34,50 @@ class App extends Component {
     document.removeEventListener("keydown", this.handleKeyDown);
   }
 
-  handleKeyDown = (e) => {
+  handleKeyDown(e) {
     if (e.ctrlKey && e.key === "h") {
       alert("Logging you out");
-      this.props.logOut(); // استدعاء الدالة يلي بتجي من props
+      this.props.logOut();
     }
-  };
+  }
 
   render() {
+    const { isLoggedIn } = this.props;
+
+    const notificationsList = [
+      { id: 1, type: "urgent", value: "New course available" },
+      { id: 2, type: "urgent", value: "New resume available" },
+      { id: 3, type: "urgent", html: { __html: getLatestNotification() } },
+    ];
+
+    const coursesList = [
+      { id: 1, name: "ES6", credit: 60 },
+      { id: 2, name: "Webpack", credit: 20 },
+      { id: 3, name: "React", credit: 40 },
+    ];
+
     return (
       <>
         <div className="root-notifications">
-          <Notifications notifications={this.notificationsList} />
+          <Notifications notifications={notificationsList} />
         </div>
         <Header />
-        {this.state.isLoggedIn ? (
-          <CourseList courses={this.coursesList} />
+        {isLoggedIn ? (
+          <BodySectionWithMarginBottom title="Course list">
+            <CourseList courses={coursesList} />
+          </BodySectionWithMarginBottom>
         ) : (
-          <Login />
+          <BodySectionWithMarginBottom title="Log in to continue">
+            <Login />
+          </BodySectionWithMarginBottom>
         )}
+        <BodySection title="News from the School">
+          <p>Holberton School News goes here</p>
+        </BodySection>
         <Footer />
       </>
     );
   }
 }
-
-// إضافة prop افتراضي إذا ما اجا logOut من برا
-App.defaultProps = {
-  logOut: () => {},
-};
-
-// تأكيد نوع الدالة
-App.propTypes = {
-  logOut: PropTypes.func,
-};
 
 export default App;
