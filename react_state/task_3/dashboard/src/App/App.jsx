356dceb80e74f346cd5bbbecf3454297fc32732c
TAREKZAINALDIN <tarekzindin@gmail.com>
2025-05-29 18:14:05 +0200

    tryy

--- react_state/task_3/dashboard/src/App/App.jsx
+++ react_state/task_3/dashboard/src/App/App.jsx
@@ -1,58 +1,43 @@
-import React from 'react';
-import { StyleSheet, css } from 'aphrodite';
-import Notifications from '../Notifications/Notifications';
-import Header from '../Header/Header';
-import Footer from '../Footer/Footer';
-import Login from '../Login/Login';
-import CourseList from '../CourseList/CourseList';
-import { getLatestNotification } from '../utils/utils';
-import BodySection from '../BodySection/BodySection';
-import BodySectionWithMarginBottom from '../BodySectionWithMarginBottom/BodySectionWithMarginBottom';
-import AppContext, { user as defaultUser } from '../Context/context';
+import React from "react";
+import { StyleSheet, css } from "aphrodite";
+import Notifications from "../Notifications/Notifications";
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
+import Login from "../Login/Login";
+import CourseList from "../CourseList/CourseList";
+import { getLatestNotification } from "../utils/utils";
+import BodySection from "../BodySection/BodySection";
+import BodySectionWithMarginBottom from "../BodySection/BodySectionWithMarginBottom";
 
-const notificationsList = [
-  { id: 1, type: 'default', value: 'New course available' },
-  { id: 2, type: 'urgent', value: 'New resume available' },
-  { id: 3, type: 'urgent', html: { __html: getLatestNotification() } },
-];
-
-const coursesList = [
-  { id: 1, name: 'ES6', credit: 60 },
-  { id: 2, name: 'Webpack', credit: 20 },
-  { id: 3, name: 'React', credit: 40 },
-];
+import newContext, { user as defaultUser } from "../Context/context";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.logIn = this.logIn.bind(this);
     this.logOut = this.logOut.bind(this);
-    this.handleKeyDown = this.handleKeyDown.bind(this);
-    this.markNotificationAsRead = this.markNotificationAsRead.bind(this);
 
     this.state = {
       displayDrawer: false,
       user: { ...defaultUser },
       logOut: this.logOut,
-      notifications: notificationsList,
-      courses: coursesList,
     };
   }
 
   componentDidMount() {
-    document.addEventListener('keydown', this.handleKeyDown);
+    document.addEventListener("keydown", this.handleKeyDown);
   }
 
   componentWillUnmount() {
-    document.removeEventListener('keydown', this.handleKeyDown);
+    document.removeEventListener("keydown", this.handleKeyDown);
   }
 
-  handleKeyDown(e) {
-    if (e.ctrlKey && e.key === 'h') {
-      alert('Logging you out');
-      this.logOut();
+  handleKeyDown = (e) => {
+    if (e.ctrlKey && e.key === "h") {
+      alert("Logging you out");
+      this.state.logOut();
     }
-  }
+  };
 
   handleDisplayDrawer = () => this.setState({ displayDrawer: true });
   handleHideDrawer = () => this.setState({ displayDrawer: false });
@@ -71,66 +56,70 @@ class App extends React.Component {
     this.setState({ user: { ...defaultUser } });
   }
 
-  markNotificationAsRead(id) {
-    console.log(`Notification ${id} has been marked as read`);
-    this.setState({
-      notifications: this.state.notifications.filter((notif) => notif.id !== id),
-    });
-  }
-
   render() {
-    const { user, displayDrawer, notifications, courses } = this.state;
+    const { user } = this.state;
+
+    const notificationsList = [
+      { id: 1, type: "urgent", value: "New course available" },
+      { id: 2, type: "urgent", value: "New resume available" },
+      { id: 3, type: "urgent", html: { __html: getLatestNotification() } },
+    ];
+
+    const coursesList = [
+      { id: 1, name: "ES6", credit: 60 },
+      { id: 2, name: "Webpack", credit: 20 },
+      { id: 3, name: "React", credit: 40 },
+    ];
 
     return (
-      <AppContext.Provider value={{ user: this.state.user, logOut: this.state.logOut }}>
-        <div className={css(styles.app)}>
-          <div className={css(styles.notifications)}>
-            <Notifications
-              notifications={notifications}
-              displayDrawer={displayDrawer}
-              handleDisplayDrawer={this.handleDisplayDrawer}
-              handleHideDrawer={this.handleHideDrawer}
-              markNotificationAsRead={this.markNotificationAsRead}
-            />
-          </div>
-          <Header />
-          {user.isLoggedIn ? (
-            <BodySectionWithMarginBottom title="Course list">
-              <CourseList listCourses={courses} />
-            </BodySectionWithMarginBottom>
-          ) : (
-            <BodySectionWithMarginBottom title="Log in to continue">
-              <Login logIn={this.logIn} />
-            </BodySectionWithMarginBottom>
-          )}
-          <BodySection title="News from the School">
-            <p>  Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book. It has
-              survived not only five centuries, but also the leap into
-              electronic typesetting, remaining essentially unchanged. It was
-              popularised in the 1960s with the release of Letraset sheets
-              containing Lorem Ipsum passages, and more recently with desktop
-              publishing software like Aldus PageMaker including versions of
-              Lorem Ipsum.</p>
-          </BodySection>
-          <Footer />
+      <newContext.Provider
+        value={{ user: this.state.user, logOut: this.state.logOut }}
+      >
+        <div className={css(styles.notifications)}>
+          <Notifications
+            notifications={notificationsList}
+            displayDrawer={this.state.displayDrawer}
+            handleDisplayDrawer={this.handleDisplayDrawer}
+            handleHideDrawer={this.handleHideDrawer}
+          />
         </div>
-      </AppContext.Provider>
+        <Header />
+        {user.isLoggedIn ? (
+          <BodySectionWithMarginBottom title="Course list">
+            <CourseList courses={coursesList} />
+          </BodySectionWithMarginBottom>
+        ) : (
+          <BodySectionWithMarginBottom title="Log in to continue">
+            <Login
+              logIn={this.logIn}
+              email={user.email}
+              password={user.password}
+            />
+          </BodySectionWithMarginBottom>
+        )}
+        <BodySection title="News from the School">
+          <p>Holberton School News goes here</p>
+        </BodySection>
+        <footer className={css(styles.footer)}>
+          <p>Copyright 2025 - Holberton School</p>
+        </footer>
+      </newContext.Provider>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  app: {
-    position: 'relative',
-    minHeight: '100vh',
-  },
   notifications: {
-    position: 'absolute',
-    right: 0,
-    top: 0,
+    position: "relative",
+  },
+  footer: {
+    padding: "1rem",
+    borderTop: "1px solid #ccc",
+    textAlign: "center",
+    fontStyle: "italic",
+    position: "fixed",
+    width: "100%",
+    bottom: 0,
   },
 });
 
