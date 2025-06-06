d83866bd85fac4138f3954ea463d57662a270dd3
TAREKZAINALDIN <tarekzindin@gmail.com>
2025-06-06 11:15:55 +0200

    try

--- react_redux-part1/task_1/dashboard/src/features/auth/authSlice.js
+++ react_redux-part1/task_1/dashboard/src/features/auth/authSlice.js
@@ -1,31 +1,30 @@
-import  {createSlice} from '@redux.js/toolkit'
+import { createSlice } from '@reduxjs/toolkit';
 
-const initialState ={
-    user:{ 
-        email:'' , 
-        password:''
-    },
-    isLoggedIn:false
+const initialState = {
+  user: {
+    email: '',
+    password: '',
+  },
+  isLoggedIn: false,
 };
 
-const authSlice = createSlice ({
-    name : 'auth',
-    initialState,
-    reducers: {
-        login:(state, action) => {
-            state.user.email = action.plyload.email;
-            state.user.password = action.plyload.password;
-            state.isLoggedIn = true;
-
-        },
-        logout:(state) => {
-
-        state.user.email = '';
-        state.user.password = '';
-        state.isLoggedIn = false;
-        }
-    }
+const authSlice = createSlice({
+  name: 'auth',
+  initialState,
+  reducers: {
+    login: (state, action) => {
+      const { email, password } = action.payload;
+      state.user.email = email;
+      state.user.password = password;
+      state.isLoggedIn = true;
+    },
+    logout: (state) => {
+      state.user.email = '';
+      state.user.password = '';
+      state.isLoggedIn = false;
+    },
+  },
 });
 
-export  const {login , logout} = authSlice.actions;
+export const { login, logout } = authSlice.actions;
 export default authSlice.reducer;
