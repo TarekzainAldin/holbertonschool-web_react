4886f7b92a0c96a64985f1f7ac2c341ae1f4274e
TAREKZAINALDIN <tarekzindin@gmail.com>
2025-06-09 18:33:45 +0200

    now trying

--- react_redux-part1/task_6/dashboard/src/pages/Login/Login.jsx
+++ react_redux-part1/task_6/dashboard/src/pages/Login/Login.jsx
@@ -1,6 +1,8 @@
 import { StyleSheet, css } from 'aphrodite';
 import WithLogging from '../../components/HOC/WithLogging';
 import useLogin from '../../hooks/useLogin';
+import { useDispatch } from 'react-redux';
+import { login } from '../../features/auth/authSlice'; // استيراد الأكشن
 
 const styles = StyleSheet.create({
   body: {
@@ -30,7 +32,10 @@ const styles = StyleSheet.create({
   },
 });
 
-function Login({ login }) {
+function Login() {
+  const dispatch = useDispatch();
+
+  // استخدام الدالة login من Redux بدلاً من prop
   const {
     email,
     password,
@@ -38,7 +43,9 @@ function Login({ login }) {
     handleChangeEmail,
     handleChangePassword,
     handleLoginSubmit,
-  } = useLogin({ onLogin: login });
+  } = useLogin({
+    onLogin: ({ email, password }) => dispatch(login({ email, password })),
+  });
 
   return (
     <form aria-label="form" onSubmit={handleLoginSubmit}>
