ce6224b66ab2ced6439f7b6854a46ff1a156efa5
TAREKZAINALDIN <tarekzindin@gmail.com>
2025-06-09 19:18:34 +0200

    trying

--- react_redux-part1/task_7/dashboard/src/components/Notifications/Notifications.jsx
+++ react_redux-part1/task_7/dashboard/src/components/Notifications/Notifications.jsx
@@ -1,66 +1,71 @@
-import React, { memo } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { StyleSheet, css } from "aphrodite";
-import closeIcon from "../../assets/close-icon.png";
-import NotificationItem from "../NotificationItem/NotificationItem";
+import { memo, useCallback } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { StyleSheet, css } from 'aphrodite';
+import closeIcon from '../../assets/close-icon.png';
+import NotificationItem from '../NotificationItem/NotificationItem';
 import {
   markNotificationAsRead,
-  showNotificationDrawer,
-  hideNotificationDrawer,
-} from "../Notifications/notificationActionCreators";
+  showDrawer,
+  hideDrawer,
+} from '../../actions/notificationActions';
 
 const styles = StyleSheet.create({
   notificationTitle: {
-    float: "right",
-    position: "absolute",
-    right: "10px",
-    top: "2px",
-    cursor: "pointer",
+    float: 'right',
+    position: 'absolute',
+    right: '10px',
+    top: '2px',
+    cursor: 'pointer',
   },
   notifications: {
-    border: "dotted",
-    borderColor: "crimson",
-    marginTop: "1%",
-    paddingLeft: "1rem",
-    marginBottom: "1rem",
-    width: "40%",
-    marginLeft: "59%",
+    border: 'dotted',
+    borderColor: 'crimson',
+    marginTop: '1%',
+    paddingLeft: '1rem',
+    marginBottom: '1rem',
+    width: '40%',
+    marginLeft: '59%',
   },
   notificationsButton: {
-    position: "absolute",
-    cursor: "pointer",
-    right: "5px",
-    top: "20px",
-    background: "transparent",
-    border: "none",
+    position: 'absolute',
+    cursor: 'pointer',
+    right: '5px',
+    top: '20px',
+    background: 'transparent',
+    border: 'none',
   },
   notificationTypeDefault: {
-    color: "blue",
+    color: 'blue',
   },
   notificationTypeUrgent: {
-    color: "red",
-  },
-  menuItem: {
-    textAlign: "right",
+    color: 'red',
   },
 });
 
 const Notifications = memo(function Notifications() {
   const dispatch = useDispatch();
-  const displayDrawer = useSelector((state) => state.ui.displayDrawer);
-  const notifications = useSelector((state) => state.notifications);
 
-  const handleDisplayDrawer = () => {
-    dispatch(showNotificationDrawer());
-  };
+  const displayDrawer = useSelector(
+    (state) => state.notifications.displayDrawer
+  );
+  const notifications = useSelector(
+    (state) => state.notifications.notifications
+  );
 
-  const handleHideDrawer = () => {
-    dispatch(hideNotificationDrawer());
-  };
+  const handleDisplayDrawer = useCallback(() => {
+    dispatch(showDrawer());
+  }, [dispatch]);
 
-  const handleMarkAsRead = (id) => {
-    dispatch(markNotificationAsRead(id));
-  };
+  const handleHideDrawer = useCallback(() => {
+    dispatch(hideDrawer());
+  }, [dispatch]);
+
+  const handleMarkAsRead = useCallback(
+    (id) => {
+      dispatch(markNotificationAsRead(id));
+    },
+    [dispatch]
+  );
 
   return (
     <>
@@ -72,37 +77,35 @@ const Notifications = memo(function Notifications() {
       </div>
       {displayDrawer && (
         <div className={css(styles.notifications)}>
-          {notifications.length > 0 ? (
-            <>
-              <p>Here is the list of notifications</p>
-              <button
-                onClick={handleHideDrawer}
-                aria-label="Close"
-                className={css(styles.notificationsButton)}
-              >
-                <img src={closeIcon} alt="close icon" />
-              </button>
-              <ul>
-                {notifications.map((notification) => (
-                  <NotificationItem
-                    key={notification.id}
-                    id={notification.id}
-                    type={notification.type}
-                    value={notification.value}
-                    html={notification.html}
-                    markAsRead={() => handleMarkAsRead(notification.id)}
-                    className={
-                      notification.type === "urgent"
-                        ? css(styles.notificationTypeUrgent)
-                        : css(styles.notificationTypeDefault)
-                    }
-                  />
-                ))}
-              </ul>
-            </>
-          ) : (
-            <p>No new notifications for now</p>
-          )}
+          <p>Here is the list of notifications</p>
+          <button
+            onClick={handleHideDrawer}
+            aria-label="Close"
+            className={css(styles.notificationsButton)}
+          >
+            <img src={closeIcon} alt="close icon" />
+          </button>
+          <ul>
+            {notifications.length > 0 ? (
+              notifications.map((notification) => (
+                <NotificationItem
+                  key={notification.id}
+                  id={notification.id}
+                  type={notification.type}
+                  value={notification.value}
+                  html={notification.html}
+                  markAsRead={() => handleMarkAsRead(notification.id)}
+                  className={
+                    notification.type === 'urgent'
+                      ? css(styles.notificationTypeUrgent)
+                      : css(styles.notificationTypeDefault)
+                  }
+                />
+              ))
+            ) : (
+              <li>No new notifications for now</li>
+            )}
+          </ul>
         </div>
       )}
     </>
