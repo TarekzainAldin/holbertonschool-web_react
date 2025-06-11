f233b5d42d632b59419813887c92a2967b3b2bc2
TAREKZAINALDIN <tarekzindin@gmail.com>
2025-06-11 18:44:32 +0200

    testinger

--- react_redux-part1/task_5/dashboard/src/pages/CourseList/CourseList.jsx
+++ react_redux-part1/task_5/dashboard/src/pages/CourseList/CourseList.jsx
@@ -1,63 +1,37 @@
-import React, { useEffect } from "react";
-import { StyleSheet, css } from "aphrodite";
-import CourseListRow from "./CourseListRow/CourseListRow";
-import WithLogging from "../../components/HOC/WithLogging";
-import { useSelector, useDispatch } from "react-redux";
-import { fetchCourses } from "../../features/courses/coursesSlice";
-
-const styles = StyleSheet.create({
-  courses: {
-    margin: "130px auto",
-    width: "90%",
-    height: "33vh",
-  },
-  table: {
-    width: "100%",
-    borderCollapse: "collapse",
-    border: "2px solid rgb(161, 161, 161)",
-  },
-  thtd: {
-    border: "2px solid rgb(161, 161, 161)",
-  },
-});
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchCourses } from '../../features/courses/coursesSlice';
+import CourseListRow from './CourseListRow/CourseListRow';
+import WithLogging from '../../components/HOC/WithLogging';
 
 function CourseList() {
   const dispatch = useDispatch();
+  const courses = useSelector((state) => state.courses.courses);
 
   useEffect(() => {
     dispatch(fetchCourses());
   }, [dispatch]);
 
-  const courses = useSelector((state) => state.courses.courses);
-
   return (
-    <div
-    // className={css(styles.courses)}
-    >
-      <table
-        id="CourseList"
-        // className={css(styles.table)}
-      >
+    <div>
+      <table id="CourseList">
         <thead>
           {courses.length > 0 ? (
             <>
               <CourseListRow
                 textFirstCell="Available courses"
                 isHeader={true}
-                //style={styles.thtd}
               />
               <CourseListRow
                 textFirstCell="Course name"
                 textSecondCell="Credit"
                 isHeader={true}
-                //style={styles.thtd}
               />
             </>
           ) : (
             <CourseListRow
               isHeader={true}
               textFirstCell="No course available yet"
-              //style={styles.thtd}
             />
           )}
         </thead>
@@ -68,7 +42,6 @@ function CourseList() {
                 key={course.id}
                 textFirstCell={course.name}
                 textSecondCell={course.credit}
-                //style={styles.thtd}
               />
             ))}
           </tbody>
