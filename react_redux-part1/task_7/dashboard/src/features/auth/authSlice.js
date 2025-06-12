66f68c9311958af5086c5ca32dd6698b24dcb876
TAREKZAINALDIN <tarekzindin@gmail.com>
2025-06-12 11:24:12 +0200

    fdsqdsqf

--- react_redux-part1/task_7/dashboard/src/features/auth/authSlice.js
+++ react_redux-part1/task_7/dashboard/src/features/auth/authSlice.js
@@ -1,29 +1,28 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
   user: {
-    email: "",
-    password: "",
+    email: '',
+    password: ''
   },
-  isLoggedIn: false,
+  isLoggedIn: false
 };
 
 const authSlice = createSlice({
-  name: "auth",
+  name: 'auth',
   initialState,
   reducers: {
     login: (state, action) => {
-      const { email, password } = action.payload;
-      state.user.email = email;
-      state.user.password = password;
+      state.user.email = action.payload.email;
+      state.user.password = action.payload.password;
       state.isLoggedIn = true;
     },
     logout: (state) => {
-      state.user.email = "";
-      state.user.password = "";
+      state.user.email = '';
+      state.user.password = '';
       state.isLoggedIn = false;
-    },
-  },
+    }
+  }
 });
 
 export const { login, logout } = authSlice.actions;
