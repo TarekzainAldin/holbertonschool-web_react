bfe52f54a2192eef681ed88d1dc304e3f6f6144f
TAREKZAINALDIN <tarekzindin@gmail.com>
2025-06-13 14:52:18 +0200

    testing the notificaiton not display true and rendering the  componenet

--- react_redux-part2/task_0/dashboard/src/features/notifications/notificationsSlice.js
+++ react_redux-part2/task_0/dashboard/src/features/notifications/notificationsSlice.js
@@ -4,7 +4,6 @@ import { getLatestNotification } from "../../utils/utils";
 
 const initialState = {
   notifications: [],
-  displayDrawer: true,
 };
 
 const API_BASE_URL = "http://localhost:5173";
@@ -56,12 +55,6 @@ const notificationsSlice = createSlice({
         (notification) => notification.id !== idToRemove
       );
     },
-    showDrawer: (state) => {
-      state.displayDrawer = true;
-    },
-    hideDrawer: (state) => {
-      state.displayDrawer = false;
-    },
   },
   extraReducers: (builder) => {
     builder.addCase(fetchNotifications.fulfilled, (state, action) => {
@@ -70,7 +63,6 @@ const notificationsSlice = createSlice({
   },
 });
 
-export const { markNotificationAsRead, showDrawer, hideDrawer } =
-  notificationsSlice.actions;
+export const { markNotificationAsRead } = notificationsSlice.actions;
 
 export default notificationsSlice.reducer;
